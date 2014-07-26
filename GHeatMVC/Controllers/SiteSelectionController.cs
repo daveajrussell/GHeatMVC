56ddcbb73dda7c9bab692eab97fabbd42e6845d2
Dave <dave@daveajrussell.com>
2014-07-26 12:25:05 +0100

    Uncommitted changes.
    Misc.

---
+++ GHeatMVC/Controllers/SiteSelectionController.cs
@@ -0,0 +1,41 @@
+﻿using GHeatMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GHeatMVC.Controllers
+{
+    public class SiteSelectionController : Controller
+    {
+        private List<SiteSelectionViewModel> items = new List<SiteSelectionViewModel>()
+        {
+            new SiteSelectionViewModel()
+            {
+                Display = "Projects",
+                URL = "http://www.daveajrussell.com/Projects"
+            },
+            new SiteSelectionViewModel()
+            {
+                Display = "Contact",
+                URL = "http://www.daveajrussell.com/Contact"
+            },
+            new SiteSelectionViewModel()
+            {
+                Display = "Blog",
+                URL = "http://daveajrussell.wordpress.com/"
+            },
+            new SiteSelectionViewModel()
+            {
+                Display = "Acknowledgements",
+                URL = "http://www.daveajrussell.com/Acknowledgements"
+            }
+        };
+
+        public ActionResult Index()
+        {
+            return PartialView("SiteSelectionPartial", items);
+        }
+    }
+}
